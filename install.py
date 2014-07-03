3ce8d459e723465647a92c11674af9e6eb136960
Kashev Dalmia <kashev.dalmia@gmail.com>
2014-07-02 23:09:34 -0700

    adding force argument to install script for forcing creation of symlinks

--- install.py
+++ install.py
@@ -3,9 +3,10 @@
 # Kashev Dalmia | @kashev | kashev.dalmia@gmail.com
 # README.md
 
-import os
-import glob
+import argparse
 import getpass
+import glob
+import os
 
 USER = getpass.getuser()
 HOME_DIR = "/home/" + USER + "/"
@@ -13,36 +14,46 @@ CURR_DIR = os.getcwd() + "/"
 
 DOT_INSTALL_FOLDERS = ["git"]
 
-def delete_and_link(source, dest):
+def delete_and_link(source, dest, force=False):
     """Delete the destination file if it exists and isn't a link,
        then create a link to the destination from the source.
     """
-    if os.path.isfile(dest) and not os.path.islink(dest):
+    if os.path.isfile(dest) and (force or not os.path.islink(dest)):
         os.remove(dest)
 
     if not os.path.isfile(dest):
         os.symlink(source, dest)
 
-def install_folder(source, dest):
+def install_folder(source, dest, force=False):
     """Symlink all the dotfiles in the source to the dest
        (if a symlink doesn't already exist)
     """
-    for f in os.listdir(source): 
-        delete_and_link(source + f, dest + f)
+    for f in os.listdir(source):
+        delete_and_link(source + f, dest + f, force)
 
 def install_sublime():
-    """Install Sublime Text 3 as it is installed through this PPA:
+    """Install Sublime Text 3 settings assuming it is installed through this PPA:
        http://www.webupd8.org/2013/07/sublime-text-3-ubuntu-ppa-now-available.html
     """
     SUBLIME_TEXT_CONFIG_LOCATION = HOME_DIR + ".config/sublime-text-3/Packages/User/"
     install_folder(CURR_DIR + "sublimetext" + "/",
                    SUBLIME_TEXT_CONFIG_LOCATION)
-    
+
 
 def main():
-    """Do all the installs."""
+    """Parse command line arguments, then do all the installs."""
+    parser = argparse.ArgumentParser(
+        description="Install dotfiles and settings from github.com/kashev/dotfiles.")
+
+    parser.add_argument("-f", "--force",
+                        help="Force creation of new symlinks.",
+                        action='store_true')
+
+    args = parser.parse_args()
+    print args.force
+
     for folder in DOT_INSTALL_FOLDERS:
-        install_folder(CURR_DIR + folder + "/", HOME_DIR)
+        install_folder(CURR_DIR + folder + "/", HOME_DIR, args.force)
     install_sublime()
 
 if __name__ == '__main__':
