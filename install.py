83c3a57872544033900d7a9a0b7c16d8b3897bdf
Kashev Dalmia <kashev.dalmia@gmail.com>
2014-07-03 00:20:11 -0700

    added logging and verbosity flag to python install script

--- install.py
+++ install.py
@@ -5,7 +5,7 @@
 
 import argparse
 import getpass
-
+import logging
 import os
 
 HOME_DIR = os.path.join("/home", getpass.getuser())
@@ -17,11 +17,18 @@ def delete_and_link(source, dest, force=False):
     """Delete the destination file if it exists and isn't a link,
        then create a link to the destination from the source.
     """
-    if os.path.isfile(dest) and (force or not os.path.islink(dest)):
-        os.remove(dest)
+    try:
+        if os.path.isfile(dest) and (force or not os.path.islink(dest)):
+            logging.info("Deleting existing %s" % dest)
+            os.remove(dest)
 
-    if not os.path.isfile(dest):
-        os.symlink(source, dest)
+        if not os.path.isfile(dest):
+            logging.info("Creating symlink %s to %s" % (source, dest))
+            os.symlink(source, dest)
+
+    except Exception as e:
+        logging.critical("Error Creating Symlink %s to %s : %s" %
+                         (source, dest, e))
 
 
 def install_folder(source, dest, force=False):
@@ -29,9 +36,11 @@ def install_folder(source, dest, force=False):
        (if a symlink doesn't already exist)
     """
     for f in os.listdir(source):
+        logging.info("Installing symlinks for %s..." % f)
         delete_and_link(os.path.join(source, f),
                         os.path.join(dest, f),
                         force)
+        logging.info("Done.")
 
 
 def install_sublime():
@@ -45,8 +54,10 @@ def install_sublime():
                                 "Packages",
                                 "User")
 
+    logging.info("Installing Sublime Text 3 Settings to %s" % ST3_LOCATION)
     install_folder(os.path.join(os.getcwd(), "sublimetext"),
                    ST3_LOCATION)
+    logging.info("Done.")
 
 
 def main():
@@ -56,10 +67,16 @@ def main():
                     "github.com/kashev/dotfiles.")
 
     parser.add_argument("-f", "--force",
-                        help="Force creation of new symlinks.",
-                        action='store_true')
+                        help="force creation of new symlinks",
+                        action="store_true")
+    parser.add_argument("-v", "--verbose",
+                        help="increase output verbosity",
+                        action="store_true")
 
     args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
 
     for folder in DOT_INSTALL_FOLDERS:
         install_folder(os.path.join(os.getcwd(), folder),
