86f6ca04e92bd0056bbc3dd79e8cc64025d680c8
Kashev Dalmia <kashev.dalmia@gmail.com>
2014-07-10 13:46:46 -0700

    warn ubuntu only

--- install.py
+++ install.py
@@ -31,7 +31,7 @@ INSTALL_PATHS = {
     "sublimetext": ST3_LOC
 }
 
-# Constants related to packages to be installed.
+# Constants related to packages to be installed. Currently Ubuntu only.
 PACKAGE_MANAGER_SUDO = ["sudo"]
 PACKAGE_MANAGER_ADD_REPO = ["add-apt-repository"]
 PACKAGE_MANAGER_UPDATE = ["apt-get", "update"]
