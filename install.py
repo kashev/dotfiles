24826673e9d2248eb30ba7c05311709b05e23ee8
Kashev Dalmia <kashev.dalmia@gmail.com>
2014-07-13 21:39:45 -0700

    generalize pep8 check, prep symlinker function for folders

--- install.py
+++ install.py
@@ -5,6 +5,7 @@
 #
 # The dotfiles installer script.
 
+
 # FUTURE IMPORTS
 from __future__ import division
 from __future__ import print_function
@@ -14,6 +15,7 @@ from __future__ import unicode_literals
 import argparse
 import logging
 import os
+import shutil
 import sys
 # As this script is meant to be installed on a fresh installation, non default
 # imports are in a try-except block to cleanly handle uninstalled dependencies.
@@ -35,10 +37,28 @@ CONFIG_FILE = "config.yaml"
 
 # FUNCTIONS
 
+def check_pep8(*files):
+    """Check that the files conform to pep8. Return True if they do."""
+    logging.info("Validating files for pep8 conformance...")
+
+    flist = [f for f in files]
+
+    pep8style = pep8.StyleGuide(quiet=True)
+    pep8result = pep8style.check_files(flist)
+
+    if pep8result.total_errors != 0:
+        logging.warning("1 or more files does not conform to pep8: {} errors."
+                        .format(pep8result.total_errors))
+        pep8result.print_statistics()
+        return False
+    else:
+        return True
+
+
 def validate_config_file(config_file):
     # Check that the configuration file exists and is both valid yaml and
     # contains the information necessary to run the installer.
-    logging.info("Validating {}".format(config_file))
+    logging.info("Validating {}...".format(config_file))
     retval = True
     try:
         with open(config_file, 'r') as f:
@@ -73,14 +93,9 @@ def validate_files():
     """
     logging.info("Validating projet...")
     retval = True
-    # pep8 this script for style.
-    logging.info("Validating {}".format(__file__))
-    pep8style = pep8.StyleGuide(quiet=True)
-    pep8result = pep8style.check_files([os.path.realpath(__file__)])
-    if pep8result.total_errors != 0:
-        logging.warning("{} does not conform to pep8 with {} errors."
-                        .format(__file__, pep8result.total_errors))
-        pep8result.print_statistics()
+    # pep8 this script for style. The return value doesn't matter for project
+    # validity, so don't use the return value.
+    check_pep8(__file__)
 
     retval &= validate_config_file(CONFIG_FILE)
 
@@ -94,11 +109,14 @@ def delete_and_link(source, dest, force=False):
         symlinks to be recreated, even if they already exist.
     """
     try:
-        if os.path.isfile(dest) and (force or not os.path.islink(dest)):
+        if os.path.lexists(dest) and (force or not os.path.islink(dest)):
             logging.info("Deleting existing {}".format(dest))
-            os.remove(dest)
+            if os.path.isfile(dest):
+                os.remove(dest)
+            elif os.path.isdir(dest):
+                shutil.rmtree(dest)
 
-        if not os.path.isfile(dest):
+        if not os.path.lexists(dest):
             logging.info("Creating symlink {} to {}".format(source, dest))
             os.symlink(source, dest)
 
