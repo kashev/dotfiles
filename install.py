e0e2631eca8ac93c15991bbdfd72c289b0dc35a1
Kashev Dalmia <kashev.dalmia@gmail.com>
2014-07-15 22:53:58 -0700

    fix gnome terminal error: trying to shutil.rmtree a symlink

--- install.py
+++ install.py
@@ -115,7 +115,7 @@ def delete_and_link(source, dest, force=False):
     try:
         if os.path.lexists(dest) and (force or not os.path.islink(dest)):
             logging.info("Deleting existing {}".format(dest))
-            if os.path.isfile(dest):
+            if os.path.isfile(dest) or os.path.islink(dest):
                 os.remove(dest)
             elif os.path.isdir(dest):
                 shutil.rmtree(dest)
