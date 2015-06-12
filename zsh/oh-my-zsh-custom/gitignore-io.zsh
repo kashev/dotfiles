5d76a667211aee8b1f4b49435e0d71b5842ab0a5
Kashev Dalmia <kashev.dalmia@gmail.com>
2015-06-11 21:59:22 -0700

    a better zsh directory structure; break out aliases and functions which are not scripts into oh-my-zsh custom

---
+++ zsh/oh-my-zsh-custom/gitignore-io.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+# dotfiles
+# Kashev Dalmia | @kashev | kashev.dalmia@gmail.com
+
+####################
+# gitignore.io CLI #
+####################
+function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
