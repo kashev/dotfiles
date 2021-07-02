e42aa05e81045091d0f32098f26768b3f5facf7a
Kashev Dalmia <kashev@soteri.io>
2021-07-02 08:54:54 -0700

    Fix vscode extension install, update extensions

--- vscode_manual/install_extensions.py
+++ vscode_manual/install_extensions.py
@@ -1,32 +1,42 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Install VSCode Extensions from file.
 #
 # Instead of writing this, could have just written:
 #     cat extensions.txt | xargs -L 1 code --install-extension
 
-from __future__ import print_function
-
 import subprocess
+
+from typing import List
 
 
 EXTENSIONS_FILE = "extensions.txt"
+
+
+def run_cmd(cmd_list : List[str], show: bool=True):
+    if show:
+        print(" ".join(cmd_list))
+    subprocess.call(cmd_list, shell=False)
+
+
+def open_code() -> None:
+    run_cmd(["code"])
 
 
 def extensions(filename):
     """ Yield each of the extensions in the given file. """
     with open(filename) as extension_file:
         for extension in extension_file:
-            yield extension
+            yield extension.strip()
 
 
 def install_extension(extension):
     """ Install the given extension. """
     cmd_list = ["code", "--install-extension", extension]
-    print(" ".join(cmd_list), end="")
-    subprocess.call(cmd_list, shell=True)
+    run_cmd(cmd_list)
 
 
 def main():
+    open_code()
     for extension in extensions(EXTENSIONS_FILE):
         install_extension(extension)
 
